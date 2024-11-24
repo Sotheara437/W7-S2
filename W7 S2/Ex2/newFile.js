8b80364300ee8783a4acb51d3e686d90556bdd59
Sotheara437 <samsotheara13@gmail.com>
2024-11-24 16:31:55 +0700

    Add files via upload

---
+++ W7 S2/Ex2/newFile.js
@@ -0,0 +1,7 @@
+<script>
+    function updateIncomeValue() { }
+    const slider = document.getElementById("incomeSlider");
+    const incomeDisplay = document.getElementById("incomeValue");
+    incomeDisplay.textContent = slider.value + "K";
+
+</script>;
